99c1cd048c19ef58320b04af9d28897ed3dd36ab
Bruno Jouhier <bruno@MacBook-Pro-de-Bruno-Jouhier.local>
2010-10-18 20:05:28 +0200

    progress on mixins: handles one to many in both directions now

--- test/test.mixin.js
+++ test/test.mixin.js
@@ -41,6 +41,39 @@ $(document).ready(function(){
   Task.is(Annotatable);
   Project.is(Annotatable);
   
+  var M1 = persistence.define('M1', {
+    seq: "INT",
+    m1: "TEXT"
+  }, true);
+  
+  var M2 = persistence.define('M2', {
+    seq: "INT",
+    m2: "TEXT"
+  }, true);
+  
+  M1.hasOne('oneM2', M2);
+  M1.hasMany('manyM2', M2, 'oneM1');
+  M1.hasMany('manyManyM2', M2, 'manyManyM1');
+  M2.hasMany('manyManyM1', M1, 'manyManyM2');
+  
+  var A1 = persistence.define('A1', {
+    a1: 'TEXT'
+  });
+  var A2 = persistence.define('A2', {
+    a2: 'TEXT'
+  });
+  var B1 = persistence.define('B1', {
+    b1: 'TEXT'
+  });
+  var B2 = persistence.define('B2', {
+    b2: 'TEXT'
+  });
+  
+  A1.is(M1);
+  A2.is(M2);
+  B1.is(M1);
+  B2.is(M2);
+  
   window.Project = Project;
   window.Task = Task
   window.Project = Project;
@@ -64,7 +97,7 @@ $(document).ready(function(){
     });
 
   
-    asyncTest("creating mixin", 7, function(){
+    asyncTest("basic mixin", 7, function(){
       var now = new Date();
       now.setMilliseconds(0);
       
@@ -82,22 +115,77 @@ $(document).ready(function(){
       n2.annotated = p;
       p.lastAnnotated = now;
       persistence.flush(function(){
-      
-      })
-      Project.all().list(function(projects){
+        Project.all().list(function(projects){
+          persistence.clean();
+          equals(projects.length, 1)
+          var p = projects[0];
+          p.notes.order('text', true).list(function(notes){
+            equals(notes.length, 2);
+            equals(notes[0].text, "note 1");
+            equals(notes[1].text, "note 2");
+            notes[0].fetch("annotated", function(source){
+              equals(p.id, source.id);
+              equals(typeof source.lastAnnotated, typeof now);
+              equals(source.lastAnnotated.getTime(), now.getTime());
+              start();
+            });
+          });     
+        });
+      });
+    });
+  
+    asyncTest("many to many with mixins", 8, function(){
+      var a1 = new A1({
+        seq: 1,
+        a1: "a1"
+      });
+      var b1 = new B1({
+        seq: 2,
+        b1: "b1"
+      });
+      var a2 = new A2({
+        seq: 3,
+        a2: "a2"
+      });
+      var a2x = new A2({
+        seq: 4,
+        a2: "a2x"
+      });
+      var a2y = new A2({
+        seq: 5,
+        a2: "a2y"
+      });
+      var b2x = new B2({
+        seq: 6,
+        b2: "b2x"
+      });
+      var b2y = new B2({
+        seq: 7,
+        b2: "b2y"
+      });
+      persistence.add(a1);
+      a1.oneM2 = b2x;
+      a1.manyM2.add(a2x);
+      a1.manyM2.add(b2x);
+      persistence.flush(function(){
         persistence.clean();
-        equals(projects.length, 1)
-        var p = projects[0];
-        p.notes.order('text', true).list(function(notes){
-          equals(notes.length, 2);
-          equals(notes[0].text, "note 1");
-          equals(notes[1].text, "note 2");
-          notes[0].fetch("annotated", function(source){
-            equals(p.id, source.id);
-            equals(typeof source.lastAnnotated, typeof now);
-            equals(source.lastAnnotated.getTime(), now.getTime());
-            start();
-          })
+        A1.all().list(function(a1s){
+          equals(a1s.length, 1, "A1 list ok")
+          var a1 = a1s[0];
+          a1.fetch("oneM2", function(m2) {
+            ok(m2 != null, "oneM2 not null");
+            equals(m2.b2, "b2x", "oneM2 ok");
+            a1.manyM2.order('seq', true).list(function(m2s) {
+              equals(m2s.length, 2, "manyM2 length ok");
+              equals(m2s[0].a2, "a2x", "manyM2[0] ok");
+              equals(m2s[1].b2, "b2x", "manyM2[1] ok");
+              m2s[1].fetch("oneM1", function(m1) {
+                ok(m1 != null, "manyM2[1].oneM1 not null");
+                ok(m1.a1, "a1", "manyM2[1].oneM1 ok");
+                start();
+              });
+            });
+          });
         });
       });
     });
